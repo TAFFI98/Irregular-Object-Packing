corrected Z coordinate calculation

env.py
@@ -270,7 +270,8 @@ class Env(object):
         
         # Extracts the boundingbox of the item
         AABB = p.getAABB(item_id)                                      
-        
+        obj_length = AABB[1][0] - AABB[0][0]
+        obj_width = AABB[1][1] - AABB[0][1]
         # Computation of a grid of points within the bounding box of the item. 
         sep_x = (AABB[1][0] - AABB[0][0])/self.resolution
         sep_y = (AABB[1][1] - AABB[0][1])/self.resolution
@@ -300,7 +301,7 @@ class Env(object):
         
         # Resets the initial orientation for the item
         p.resetBasePositionAndOrientation(item_id,old_pos,old_quater)
-        return Ht,Hb
+        return Ht,Hb, obj_length, obj_width
     
     def order_by_bbox_volume(self,items_ids):
         '''
@@ -630,7 +631,7 @@ class Env(object):
         print('------------------Reward  is: ', reward,'  ------------------')
         return reward
     
-    def get_z(self,Hc,Hb,pixel_x,pixel_y):
+    def get_z(self,Hc,Hb,pixel_x,pixel_y,obj_length,obj_width):
         '''
         Hc: numpy array of shape (resolution, resolution) representing the current box heightmap
         Hb: numpy array of shape (resolution, resolution) representing the heightmap of the item
@@ -640,17 +641,23 @@ class Env(object):
         
         Function to compute the maximum z-value of the item in the box due to gravity
         '''
-        w, h = Hc.shape
-        s_range = range(-w//2, w//2)
-        t_range = range(-h//2, h//2)
+
+        s_range = range(-Hb.shape[0]//2, Hb.shape[0]//2)
+        t_range = range(-Hb.shape[1]//2, Hb.shape[1]//2)
         max_z = float('-inf')
+        ss_range = range(-(int(self.resolution*obj_length//self.box_size[0]))//2, int((self.resolution*obj_length//self.box_size[0]))//2)
+        tt_range = range(-(int(self.resolution*obj_width//self.box_size[1]))//2, int((self.resolution*obj_width//self.box_size[1]))//2)
+
         if np.all(Hc == 0): # No items in the box
             max_z = 0
         else:
             for s in s_range:
                 for t in t_range:
-                        z = Hc[pixel_x, pixel_y] - Hb[s, t]
-                        max_z = max(max_z, z)
+                        for ss in ss_range:
+                            for tt in tt_range:
+                                if 0 <= pixel_x+ss < Hc.shape[0] and 0 <= pixel_y+tt < Hc.shape[1]:
+                                    z = Hc[pixel_x+ss, pixel_y+tt] - Hb[s, t]
+                                    max_z = max(max_z, z)
         return max_z
 
 
@@ -738,6 +745,7 @@ if __name__ == '__main__':
 
 
     
+
 
 
 

test_train_models.py
@@ -31,7 +31,7 @@ in training mode after each object is packed the snapshot is saved.
 '''
 if __name__ == '__main__':
         
-    is_testing = 'False'
+    is_testing = 'False' # True or False
     #-- Path with the URDF files
     obj_folder_path = '/Project/Irregular-Object-Packing/objects/'
     
@@ -101,7 +101,7 @@ if __name__ == '__main__':
                     six_views_one_item = []
                     for view in principal_views.values():
                         if id_ in env.unpacked:
-                            Ht,_ = env.item_hm(id_, view)
+                            Ht,_,obj_length,obj_width  = env.item_hm(id_, view)
                             #env.visualize_object_heightmaps(id_, view, only_top = True )
                             #env.visualize_object_heightmaps_3d(id_, view, only_top = True )
                             six_views_one_item.append(Ht) 
@@ -143,7 +143,7 @@ if __name__ == '__main__':
                         orient = [roll, pitch, 0]
 
                         roll_pitch_angles.append(np.array([roll,pitch]))
-                        Ht,Hb = env.item_hm(chosen_item_index, orient )
+                        Ht,Hb, obj_length, obj_width= env.item_hm(chosen_item_index, orient )
                         #env.visualize_object_heightmaps(chosen_item_index, orient)
                         #env.visualize_object_heightmaps_3d(chosen_item_index,orient )
                         Ht.shape = (Ht.shape[0], Ht.shape[1], 1)
@@ -225,7 +225,7 @@ if __name__ == '__main__':
                     six_views_one_item = []
                     for view in principal_views.values():
                         if id_ in env.unpacked:
-                            Ht,_ = env.item_hm(id_, view)
+                            Ht,_,obj_length,obj_width = env.item_hm(id_, view)
                             #env.visualize_object_heightmaps(id_, view, only_top = True )
                             #env.visualize_object_heightmaps_3d(id_, view, only_top = True )
                             six_views_one_item.append(Ht) 
@@ -261,7 +261,7 @@ if __name__ == '__main__':
                         orient = [roll, pitch, 0]
 
                         roll_pitch_angles.append(np.array([roll,pitch]))
-                        Ht,Hb = env.item_hm(chosen_item_index, orient )
+                        Ht,Hb, obj_length,obj_width= env.item_hm(chosen_item_index, orient )
                         #env.visualize_object_heightmaps(chosen_item_index, orient)
                         #env.visualize_object_heightmaps_3d(chosen_item_index,orient )
                         Ht.shape = (Ht.shape[0], Ht.shape[1], 1)
@@ -314,4 +314,39 @@ if __name__ == '__main__':
         print('Number of packed items: ', len(env.packed))
 
 
-    
+    """ 
+    Visualize rotated Heightmaps 
+    yaw_angles = np.arange(0,2*np.pi,np.pi/2)*180/np.pi
+    num_yaws = yaw_angles.shape[0]
+    item_heightmaps_RPY = np.empty(shape=(num_yaws, num_rp, item_heightmaps_RP.shape[1],item_heightmaps_RP.shape[2], 2)) # shape: (num_yaws, num_RP, resolution, resolution, 2)
+
+    for j,yaw in enumerate(yaw_angles):
+        for i in range(num_rp):
+                    # Rotate Ht
+                    Ht = item_heightmaps_RP[i,:,:,0]
+                    rotated_Ht = rotate(Ht, yaw, reshape=False)
+                    item_heightmaps_RPY[j,i,:,:,0] = rotated_Ht
+
+                    # Rotate Hb
+                    Hb = item_heightmaps_RP[i,:,:,1]
+                    rotated_Hb = rotate(Hb, yaw, reshape=False)
+                    item_heightmaps_RPY[j,i,:,:,1] = rotated_Hb 
+
+    #-- conv_block visualize
+    batch_size = 1
+    conv_block_graph = draw_graph(conv_block(in_c=3,out_c=1), input_size=(batch_size,3,200,200),graph_name = 'conv_block',save_graph= True, directory= '/Project/Irregular-Object-Packing/models_plot/', graph_dir ='TB')
+    conv_block_graph.visual_graph
+
+    #-- Downsample visualize
+    Downsample_graph = draw_graph(Downsample(channel=1), input_size=(batch_size,1,32,32),graph_name = 'Downsample',save_graph= True, directory= '/Project/Irregular-Object-Packing/models_plot/', graph_dir ='LR')
+    Downsample_graph.visual_graph
+
+    #-- Upsample visualize
+    Upsample_graph = draw_graph(Upsample(channel=3), input_data=[torch.rand(batch_size,3,32,32), torch.rand(batch_size,3,64,64)],graph_name = 'Upsample',save_graph= True, directory= '/Project/Irregular-Object-Packing/models_plot/',  graph_dir ='LR')
+    Upsample_graph.visual_graph""" 
+
+
+
+
+
+

trainer.py
@@ -262,8 +262,8 @@ class Trainer(object):
             # Pack chosen item with predicted pose
             target_euler = [r,p,y]
             # Compute z coordinate
-            _,Hb_selected_obj = env.item_hm(chosen_item_index, target_euler )
-            z = env.get_z(BoxHeightMap,Hb_selected_obj,pixel_x, pixel_y)
+            _,Hb_selected_obj,obj_length,obj_width = env.item_hm(chosen_item_index, target_euler )
+            z = env.get_z(BoxHeightMap,Hb_selected_obj,pixel_x, pixel_y,obj_length,obj_width)
             target_pos = [pixel_x * env.box_size[0]/env.resolution,pixel_y * env.box_size[1]/env.resolution, z] # m
             transform = np.empty(6,)
             transform[0:3] = target_euler
@@ -343,3 +343,4 @@ class Trainer(object):
         # If there's a manager network, save it as well
         if self.method == 'stage_2':
             torch.save(self.manager_network.state_dict(), f'snapshots/manager_network_{self.epoch}.pth')
+